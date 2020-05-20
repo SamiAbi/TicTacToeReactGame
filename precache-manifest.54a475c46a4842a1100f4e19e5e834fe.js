ab7f8d26978e37195af30828277db1a681cc8d1d
Sami Abishai <Sami.Abishai@clicktale.com>
2020-05-20 18:39:40 +0300

    Updates

---
+++ precache-manifest.54a475c46a4842a1100f4e19e5e834fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a741552779313ee441777429c2bec9df",
+    "url": "/TicTacToeReactGame/index.html"
+  },
+  {
+    "revision": "69d2f400fe18726cf580",
+    "url": "/TicTacToeReactGame/static/js/2.8b31d850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TicTacToeReactGame/static/js/2.8b31d850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f661750963f616c14ec",
+    "url": "/TicTacToeReactGame/static/js/main.b70030d9.chunk.js"
+  },
+  {
+    "revision": "3f129f0d1f2633e685f7",
+    "url": "/TicTacToeReactGame/static/js/runtime-main.fa713e53.js"
+  }
+]);
